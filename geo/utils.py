15a8f3441f6acdaae7ba48b64d13b4520799f6a8
0423chw <0423chw@gmail.com>
2024-11-16 23:24:00 +0900

    Lab11

--- geo/utils.py
+++ geo/utils.py
@@ -1,3 +1,6 @@
-def some_geo_function():
-    # 함수 구현
-    pass
+# utils.py
+import math
+
+def calculate_distance(x1, y1, x2, y2):
+    """두 점 사이의 거리를 계산하는 함수"""
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
