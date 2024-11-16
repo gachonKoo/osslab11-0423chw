808f75183a5bb555ad55928f8e1e94b12761f3b3
0423chw <0423chw@gmail.com>
2024-11-16 22:14:23 +0900

    tester.py

--- tester.py
+++ tester.py
@@ -1,5 +1,9 @@
-assignment-repo/
-├── geo/
-│   ├── __init__.py
-│   └── utils.py
-└── tester.py
+from geo import utils
+
+def test_calculate_distance():
+    assert utils.calculate_distance(0, 0, 3, 4) == 5, "거리 계산 오류"
+    assert utils.calculate_distance(1, 1, 4, 5) == 5, "거리 계산 오류"
+
+if __name__ == "__main__":
+    test_calculate_distance()
+    print("모든 테스트가 통과되었습니다!")
